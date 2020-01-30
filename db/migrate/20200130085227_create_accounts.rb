3c40e856785059f82575838918d667280bf3a626
man <vanman.lqd@gmail.com>
2020-01-30 15:57:22 +0700

    create table: accounts

---
+++ db/migrate/20200130085227_create_accounts.rb
@@ -0,0 +1,15 @@
+class CreateAccounts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounts do |t|
+
+      t.bigint :user_id
+      t.string :email
+      t.string :password_digest
+      t.boolean :is_activated
+      t.datetime :activated_at
+      t.string :activation_code
+      t.string :activation_code_expired_at
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_085003) do
+ActiveRecord::Schema.define(version: 2020_01_30_085227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_activated"
+    t.datetime "activated_at"
+    t.string "activation_code"
+    t.string "activation_code_expired_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
