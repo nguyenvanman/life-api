4ee28b6d7fdee47076d1db64f643b2045e49ad45
Nhi Nguyen <nhinguyen@novahub.vn>
2020-02-24 15:09:33 +0700

    format-code

--- app/services/user_service/update.rb
+++ app/services/user_service/update.rb
@@ -1,6 +1,6 @@
 module UserService::Update 
-    def self.call(user, user_params )
-    user.update!(user_params)
-    user
-    end
+  def self.call(user, user_params )
+  user.update!(user_params)
+  user
   end
+end
