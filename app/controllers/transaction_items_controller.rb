e1bd984179ba9e30f29bb2fca4bff0424ff2021d
Nhi Nguyen <nhinguyen@novahub.vn>
2020-02-25 15:08:21 +0700

    add-transaction-type-in-to-transaction-params

--- app/controllers/transaction_items_controller.rb
+++ app/controllers/transaction_items_controller.rb
@@ -1,11 +1,12 @@
 class TransactionItemsController < ApplicationController
 
   def create
-    render_data TransactionItemService::Create.call(@current_user, transaction_item_params)
+    transaction_item = TransactionItemSerializer.new(TransactionItemService::Create.call(@current_user, transaction_item_params))
+    render_data transaction_item
   end
 
   def transaction_item_params
-    params.require(%i[amount time note categories])
-    params.permit(:amount, :time, :note,  categories: [])
+    params.require(%i[amount time note transaction_type categories])
+    params.permit(:amount, :time, :note, :transaction_type, categories: [])
   end
 end
