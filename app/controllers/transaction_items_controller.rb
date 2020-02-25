b7ce1525dfbedee1193983a7bcba18988b6b128d
Nhi Nguyen <nhinguyen@novahub.vn>
2020-02-25 16:34:29 +0700

    add-render-one-in-transaction-item

--- app/controllers/transaction_items_controller.rb
+++ app/controllers/transaction_items_controller.rb
@@ -1,8 +1,16 @@
 class TransactionItemsController < ApplicationController
 
   def create
-    transaction_item = TransactionItemSerializer.new(TransactionItemService::Create.call(@current_user, transaction_item_params))
-    render_data transaction_item
+    transaction_item = TransactionItemService::Create.call(@current_user, transaction_item_params)
+    render_one transaction_item
+  end
+
+  def render_one transaction_item
+    data = {
+      transaction_item: TransactionItemSerializer.new(transaction_item)
+    }
+
+    render_data data
   end
 
   def transaction_item_params
