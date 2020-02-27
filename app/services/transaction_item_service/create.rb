29de0b15771102dc9389b5a3a6630b6cae9de429
Nhi Nguyen <nhinguyen@novahub.vn>
2020-02-27 15:45:02 +0700

    update-user-config-after-transaction

--- app/services/transaction_item_service/create.rb
+++ app/services/transaction_item_service/create.rb
@@ -2,9 +2,9 @@ module TransactionItemService::Create
   def self.call(user, transaction_item_params)
       transaction_item = user.transaction_items.create!(transaction_item_params.except(:categories))
       if transaction_item.transaction_type.eql?('income')
-        user.config.current_budget +=transaction_item.amount
+        user.config.current_budget += transaction_item.amount
       else
-        user.config.current_budget -=transaction_item.amount
+        user.config.current_budget -= transaction_item.amount
       end
       user.config.save
       categories = transaction_item_params[:categories]
