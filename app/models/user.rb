497545a3f7d871885d4dc9203d72d9ac3d92f038
Nhi Nguyen <nhinguyen@novahub.vn>
2020-02-27 16:21:58 +0700

    update-user-config-after-transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_one :config, dependent: :destroy, class_name: 'UserConfig'
   has_many :transaction_items, dependent: :destroy
   validates :name, presence: true
+
+  def update_budget transaction_item
+    if transaction_item.income?
+      config.increase_budget transaction_item.amount
+    else
+      config.decrease_budget transaction_item.amount
+    end
+  end
 end
--- app/services/transaction_item_service/create.rb
+++ app/services/transaction_item_service/create.rb
@@ -1,11 +1,7 @@
 module TransactionItemService::Create
   def self.call(user, transaction_item_params)
       transaction_item = user.transaction_items.create!(transaction_item_params.except(:categories))
-      if transaction_item.income?
-        user.config.increase_budget transaction_item.amount
-      else
-        user.config.decrease_budget transaction_item.amount
-      end
+      user.update_budget transaction_item
       categories = transaction_item_params[:categories]
       transaction_item.categories << categories.map do |category|
         CategoryService::FindOrCreate.call(category)
