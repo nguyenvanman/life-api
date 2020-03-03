4ff346399f4301f7d79ea3c8d8109883f26f8f32
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-03 13:57:57 +0700

    get transaction by category

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
       scope :me do
         get '', to: 'users#show'
         put '', to: 'users#update'
-        resources :transaction_items, only: %i[create index]
-        resources :categories, only: %i[index]
+        resources :transaction_items, only: %i[create index] 
+        resources :categories, only: %i[index] do
+          get 'transactions', to:'transaction_items#get_by_category'
+        end
       end
     end
   end
