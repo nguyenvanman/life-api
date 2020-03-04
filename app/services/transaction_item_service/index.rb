3a1632264ddddbe7c7cc66a8a71c465d5098450c
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-04 10:10:51 +0700

    fix get transaction by time

--- app/services/transaction_item_service/index.rb
+++ app/services/transaction_item_service/index.rb
@@ -2,9 +2,9 @@ module TransactionItemService::Index
   def self.call(date, type)
     date = (date || DateTime.now).to_datetime
     if type == 'week'
-      TransactionItem.includes(:categories).mine.in_week(date)
+      TransactionItem.includes(:categories).mine.in_year(date).in_week(date)
     elsif type == 'month'
-      TransactionItem.includes(:categories).mine.in_month(date)
+      TransactionItem.includes(:categories).mine.in_year(date).in_month(date)
     else
       TransactionItem.includes(:categories).mine.in_date(date)
     end
