5e26fdfbe800e80246d6baef2a6253363e71b767
nguyenvanman <vanman.lqd@gmail.com>
2020-06-13 08:51:58 +0700

    increase token expired time

--- app/services/util_service/json_web_token.rb
+++ app/services/util_service/json_web_token.rb
@@ -2,7 +2,7 @@ module UtilService
   class JsonWebToken
     HMAC_SECRET = Rails.application.secrets.secret_key_base
   
-    def self.encode(payload, exp = 24.hours.from_now)
+    def self.encode(payload, exp = 6.months.from_now)
       payload[:exp] = exp.to_i  
       return JWT.encode(payload, HMAC_SECRET), exp
     end
